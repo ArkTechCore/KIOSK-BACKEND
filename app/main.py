fd274d974424f3e175971a49f563101938ff9439
QuickFoods <quickfoods.desk@gmail.com>
2026-01-13 01:32:56 -0500

    Add tax_rate to stores page

--- app/main.py
+++ app/main.py
@@ -51,8 +51,7 @@ app.add_middleware(
     CORSMiddleware,
    allow_origins=[
   "http://localhost:3000",
-  "https://qfkiosk.netlify.app",
-  "https://www.qfkiosk.netlify.app",
+  "https://kiosk-admin-axtb.onrender.com",
 ],
     allow_credentials=True,
     allow_methods=["*"],
